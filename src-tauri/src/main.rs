c36e31f102259fdba245ac9ab8e6f1690e41554e
Relkondo <samuel.coron@hotmail.fr>
2024-01-31 15:46:17 -0500

    Using Tauri's shortcut registering rather than my lousy listen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::collections::HashSet;
 use tauri::generate_context;
-use std::sync::{Arc, Mutex};
-use rdev::{EventType, Key, listen};
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
 
 mod execute;
 
@@ -14,51 +10,36 @@ static IS_LISTENING: AtomicBool = AtomicBool::new(false);
 static IS_RUNNING: AtomicBool = AtomicBool::new(false);
 
 fn main() {
-    open_listening_channel();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![start_listening, stop_listening, greet])
+        .invoke_handler(tauri::generate_handler![activate, deactivate, execute, greet])
         .run(generate_context!())
         .expect("error while running tauri application");
 }
 
-fn open_listening_channel() {
-    thread::spawn(move || {
-        println!("Opening global keyboard events listening channel...");
-        let pressed_keys = Arc::new(Mutex::new(HashSet::new()));
-        match listen(move |event| {
-            let mut keys = pressed_keys.lock().unwrap();
-            match event.event_type {
-                EventType::KeyPress(key) => {
-                    keys.insert(key);
-                    if keys.contains(&Key::ControlLeft) && keys.contains(&Key::KeyG) {
-                        println!("Ctrl+G pressed!");
-                        if IS_LISTENING.load(Ordering::SeqCst) && !IS_RUNNING.load(Ordering::SeqCst) {
-                            IS_RUNNING.store(true, Ordering::SeqCst);
-                            execute::execute();
-                            IS_RUNNING.store(false, Ordering::SeqCst);
-                        }
-                    }
-                }
-                EventType::KeyRelease(key) => {
-                    keys.remove(&key);
-                }
-                _ => (),
-            }
-        }) {
-            Ok(_) => println!("Channel for global keyboard events opened."),
-            Err(e) => println!("Error while opening: {:?}", e),
-        }
-    });
+#[tauri::command]
+fn execute() {
+    if IS_LISTENING.load(Ordering::SeqCst) && !IS_RUNNING.load(Ordering::SeqCst) {
+        IS_RUNNING.store(true, Ordering::SeqCst);
+        execute::execute();
+        IS_RUNNING.store(false, Ordering::SeqCst);
+    } else if !IS_LISTENING.load(Ordering::SeqCst) {
+        println!("Received signal but FlashMem is not activated.");
+    } else {
+        println!("Received signal but FlashMem is already running.");
+    }
+
 }
 
 #[tauri::command]
-fn start_listening() {
+fn activate() {
     IS_LISTENING.store(true, Ordering::SeqCst);
+    println!("Activated FlashMem.");
 }
 
 #[tauri::command]
-fn stop_listening() {
+fn deactivate() {
     IS_LISTENING.store(false, Ordering::SeqCst);
+    println!("Deactivated FlashMem.");
 }
 
 #[tauri::command]
--- src/lib/Execute.svelte
+++ src/lib/Execute.svelte
@@ -1,13 +1,28 @@
 <script>
 	import { invoke } from '@tauri-apps/api/tauri'
+	import { register } from '@tauri-apps/api/globalShortcut';
+	import { onMount } from 'svelte';
 
 	let running = false;
 
+	onMount(async () => {
+		console.log('Registering shortcut...');
+		try {
+			await register('Ctrl+G', () => {
+				console.log('Ctrl+G pressed');
+				invoke('execute');
+			});
+			console.log('Shortcut Ctrl+G registered');
+		} catch (error) {
+			console.error('Error registering shortcut:', error);
+		}
+	});
+
 	async function handle_click() {
 		if (running) {
-			await invoke('stop_listening');
+			await invoke('deactivate');
 		} else {
-			await invoke('start_listening');
+			await invoke('activate');
 		}
 		running = !running;
 	}
