a7da6c36795d3e823dad667b73366e79e870ce9e
Relkondo <samuel.coron@hotmail.fr>
2024-02-04 09:22:03 -0500

    Remove Execute + Make LanguagePicker -> SettingsPicker reusable

--- src/lib/SettingsPicker.svelte
+++ src/lib/SettingsPicker.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
-
-	let languages = ['English', 'French', 'Spanish', 'German', 'Italian'];
-	let search = 'English';
+	export let items: string[] = [];
+	export let label = '';
+	export let placeholder = '';
+	export let defaultPick = '';
+	let search = defaultPick;
 	let showList = false;
 	let dropdownElement: HTMLDivElement;
 
-	$: filteredLanguages = languages.filter(lang =>
-		lang.toLowerCase().includes(search.toLowerCase())
+	$: filteredItems = items.filter(i =>
+		i.toLowerCase().includes(search.toLowerCase())
 	);
 
 	onMount(() => {
@@ -18,14 +20,14 @@
 		document.removeEventListener('click', handleClickOutside);
 	});
 
-	function selectLanguage(lang: string) {
-		search = lang;
+	function selectItem(item: string) {
+		search = item;
 		showList = false;
 	}
 
 	function closeDropdown() {
 		showList = false;
-		if (!filteredLanguages.includes(search)) {
+		if (!filteredItems.includes(search)) {
 			search = 'English';
 		}
 	}
@@ -42,11 +44,11 @@
 	}
 </script>
 <div bind:this={dropdownElement} class="flex items-center bg-gray-800 space-x-3 p-3 rounded-md h-14" on:blur={closeDropdown} tabindex="-1">
-		<label for="search" class="text-white w-1/4">Translate to...</label>
+		<label for="search" class="text-white w-1/4">{label}</label>
 		<div class="relative w-1/2">
 			<input
 				type="text"
-				placeholder="Pick a target language..."
+				placeholder="{placeholder}"
 				bind:value={search}
 				class="bg-gray-700 w-full text-white px-4 py-2 rounded"
 				on:focus={() => showList = true}
@@ -54,8 +56,8 @@
 			/>
 			{#if showList}
 				<div class="absolute flex flex-col w-full bg-gray-700 max-h-60 overflow-auto rounded-b-md z-50">
-					{#each filteredLanguages as language}
-						<button class="text-white text-left p-2 hover:bg-green-600 cursor-pointer" on:click={() => selectLanguage(language)}>{language}</button>
+					{#each filteredItems as item}
+						<button class="text-white text-left p-2 hover:bg-green-600 cursor-pointer" on:click={() => selectItem(item)}>{item}</button>
 					{/each}
 				</div>
 			{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,63 @@
 <script>
-	import Execute from '$lib/Execute.svelte';
 	import "../app.css";
-	import LanguagePicker from '$lib/LanguagePicker.svelte';
+	import SettingsPicker from '$lib/SettingsPicker.svelte';
 	import CroppingSetting from '$lib/CroppingSetting.svelte';
+	import { onMount, onDestroy } from 'svelte';
+	import { register, unregisterAll } from '@tauri-apps/api/globalShortcut';
+	import { invoke } from '@tauri-apps/api/tauri';
+	import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/api/notification';
+
+	const TITLE = "FlashMem Translated Sub";
+
+	let languages = ['English', 'French', 'Spanish', 'German', 'Italian'];
+
+	onMount(async () => {
+		await register_shortcut();
+	});
+
+	onDestroy(async () => {
+		await unregisterAll();
+	});
+
+	async function register_shortcut() {
+		try {
+			await unregisterAll();
+			await register('Ctrl+G', execute);
+			console.log('Shortcut Ctrl+G registered');
+		} catch (error) {
+			console.error('Error registering shortcut:', error);
+		}
+	}
+
+	async function execute() {
+		console.log('Ctrl+G pressed');
+		let notification = await invoke('execute');
+		if (notification !== "###-Already Running-###") {
+			console.log('Sending notification...');
+			await send_notification(TITLE, notification);
+		} else {
+			console.log('Cannot send notification, previous notification still in progress...');
+		}
+	}
+
+	/** @param {string} title
+	 *  @param {string} notification */
+	async function send_notification(title, notification) {
+		let permissionGranted = await isPermissionGranted();
+		if (!permissionGranted) {
+			const permission = await requestPermission();
+			permissionGranted = permission === 'granted';
+		}
+		if (permissionGranted) {
+			sendNotification({ title: title, body: notification });
+		}
+	}
+
 </script>
 
 <div class="min-h-screen bg-gray-900 p-8 flex flex-col items-center">
-			<div class="mb-8 p-4">
-				<Execute />
-			</div>
 			<div class="w-full space-y-3 max-w-md">
-					<LanguagePicker />
+					<SettingsPicker items={languages} label="Translate to..." placeholder="Pick a target language..." defaultPick="English"/>
 					<CroppingSetting />
 			</div>
 </div>
