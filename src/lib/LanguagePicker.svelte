a53e50954b5bf6d3245b905e5979906269b62fac
Relkondo <samuel.coron@hotmail.fr>
2024-02-03 22:13:08 -0500

    Fixed language picker bugs

--- src/lib/LanguagePicker.svelte
+++ src/lib/LanguagePicker.svelte
@@ -11,29 +11,31 @@
 	}
 
 	function handleBlur() {
-		setTimeout(() => { showList = false; }, 100);
+		showList = false;
 	}
 
-	function handleDoubleClick() {
+	function handleClick() {
 		showList = true;
 	}
 </script>
 
-<div class="relative" on:blur={handleBlur} tabindex="-1">
-	<label for="search" class="block text-white mb-2">Translate to...</label>
-	<input
-		type="text"
-		placeholder="Pick a target language"
-		bind:value={search}
-		class="w-full bg-gray-800 text-white p-2 rounded"
-		on:focus={() => showList = true}
-		on:dblclick={handleDoubleClick}
-	/>
-	{#if showList}
-		<div class="absolute w-full bg-gray-800 max-h-60 overflow-auto rounded-b-md z-50">
-			{#each filteredLanguages as language}
-					<button class="text-white text-left p-2 hover:bg-green-600 cursor-pointer w-full" on:click={() => selectLanguage(language)}>{language}</button>
-			{/each}
+<div class="flex items-center space-x-2" on:blur={handleBlur} tabindex="-1">
+		<label for="search" class="block text-white w-1/4">Translate to...</label>
+		<div class="relative w-1/2">
+			<input
+				type="text"
+				placeholder="Pick a target language"
+				bind:value={search}
+				class="bg-gray-800 w-full text-white p-2 rounded"
+				on:focus={() => showList = true}
+				on:click={handleClick}
+			/>
+			{#if showList}
+				<div class="absolute flex flex-col w-full bg-gray-800 max-h-60 overflow-auto rounded-b-md z-50">
+					{#each filteredLanguages as language}
+						<button class="text-white text-left p-2 hover:bg-green-600 cursor-pointer" on:click={() => selectLanguage(language)}>{language}</button>
+					{/each}
+				</div>
+			{/if}
 		</div>
-	{/if}
 </div>
