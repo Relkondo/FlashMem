fb25f129878a352d72741aeb1979d743fa12737f
Relkondo <samuel.coron@hotmail.fr>
2024-03-03 09:59:52 -0500

    Added menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,10 @@
 	import "../app.css";
 </script>
 
+<nav>
+	<a href="/">Home</a>
+	<a href="/saved_subs">Saved Subs</a>
+	<a href="/advanced_settings">Advanced Settings</a>
+</nav>
+
 <slot />
---
+++ src/routes/advanced_settings/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Advanced Settings</h1>
